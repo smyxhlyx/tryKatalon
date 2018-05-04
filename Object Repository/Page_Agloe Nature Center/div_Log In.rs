d98d10b57163ec3d98774e85825970319108442d
qshen <qshen>
2018-05-04 14:05:52 -0500

    u

---
+++ Object Repository/Page_Agloe Nature Center/div_Log In.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Log In</name>
+   <tag></tag>
+   <elementGuidId>f7f81e96-ce84-4d60-bfa8-8a1fcca5b33c</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-md-6 col-sm-6 text-right clearfix</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Log In</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;fl-preset-agloe-nature fl-full-width fl-search-active&quot;]/div[@class=&quot;fl-page&quot;]/div[@class=&quot;fl-page-bar&quot;]/div[@class=&quot;fl-page-bar-container container&quot;]/div[@class=&quot;fl-page-bar-row row&quot;]/div[@class=&quot;col-md-6 col-sm-6 text-right clearfix&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
